867ae9960fcfed7d74614f4ea74420b2a3115221
Adam Horvath <horada1991@gmail.com>
2016-05-04 00:25:55 +0200

    Calculator.py was commented

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
-while 1:
+while 1:                                #infinite loop
     number1 = input("Enter a number (or a letter to" + "\033[1m" + " exit" + "\033[0m" + "): ")
     if not number1.isdigit():
-        break
+        break                           #IF there's any letter in the input than breaking the infinite while loop!
     else:
         operation = input("Enter an operation: ")
         number2 = input("Enter another number: ")
@@ -14,4 +14,4 @@ while 1:
         elif operation == "/":
             print(int(number1) / int(number2))
         else:
-            print("Please enter a valid operation!")
+            print("Please enter a valid operation!")    #If there's no valid operation input than write this
