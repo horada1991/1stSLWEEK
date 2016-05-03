c15cdb002b36193d4c1222430968b4aa289ec15a
Adam Horvath <horada1991@gmail.com>
2016-05-04 00:02:19 +0200

    Working Hello World script with default argument value in function

---
+++ HelloWorld_v2.py
@@ -0,0 +1,10 @@
+import sys
+
+def printwithname(name=[" World"]):
+    print("Hello", end="")
+    for i in name:
+        print(" " + i, end = "")
+    print("!")
+
+name = sys.argv[1:]
+printwithname() if len(name) == 0 else printwithname(name)
