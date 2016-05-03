8e3b4aa550f9c93113bfa4f759d7f72e5082c223
Adam Horvath <horada1991@gmail.com>
2016-05-04 00:49:55 +0200

    Catch Errors (ValueError -- not number input for 'number2')

---
+++ calculator_v2.py
@@ -0,0 +1,23 @@
+#infinite loop
+while 1:
+    number1 = input("Enter a number (or a letter to" + "\033[1m" + " exit" + "\033[0m" + "): ")
+    if not number1.isdigit():
+        #IF there's any not_number character in the input, than breaking the infinite while loop!
+        break
+    else:
+        operation = input("Enter an operation: ")
+        number2 = input("Enter another number: ")
+        try:
+            if operation == "+":
+                print(int(number1) + int(number2))
+            elif operation == "*":
+                print(int(number1) * int(number2))
+            elif operation == "-":
+                print(int(number1) - int(number2))
+            elif operation == "/":
+                print(int(number1) / int(number2))
+            else:
+                #If there's no valid operation input, than write this
+                print("Please enter a valid operation!")
+        except:
+            print("I can do calculation only with numbers! THANK YOU!")
