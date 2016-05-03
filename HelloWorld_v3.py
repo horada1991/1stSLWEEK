23618d2c11a9f8eb094fa253209b6975fd350e42
Adam Horvath <horada1991@gmail.com>
2016-05-04 00:07:38 +0200

    Hello World version 3 ('in 1 funct')

---
+++ HelloWorld_v3.py
@@ -0,0 +1,12 @@
+import sys
+
+def printwithname(name):
+    if len(name) == 0:
+        name = ["World"]
+    print("Hello", end="")
+    for i in name:
+        print(" " + i, end = "")
+    print("!")
+
+name = sys.argv[1:]
+printwithname(name)
