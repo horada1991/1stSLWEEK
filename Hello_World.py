da4cfdf30edf02fe154ef168d669f9855a500ee7
Adam Horvath <horada1991@gmail.com>
2016-05-07 12:43:20 +0200

    Just formatted the layout of the texts

--- Hello_World.py
+++ Hello_World.py
@@ -1,12 +1,13 @@
 import sys
 
 #funct of Print, with command line arguments
-def printwithname(name):
+def PrintWithName(name):
     print("Hello", end = "")
     for i in name:
         print(" " + i, end = "")
     print("!")
 
-printwithname(sys.argv[1:]) if sys.argv[1:] else print("Hello World!")
+PrintWithName(sys.argv[1:]) if sys.argv[1:] else print("Hello World!")
+
 #It could be solved in one logical line by the .join funct
 # but you asked for a new definied funct :D
